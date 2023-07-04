bca318eb7f8e690c4529271ca414a5d588912cd2
qntmcube <100043837+qntmcube@users.noreply.github.com>
2023-07-04 16:59:56 -0700

    made code worse but files easier to read

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ file1 = open('file.txt', 'r')
 lines = file1.readlines()
 item = []
 number = []
+c = 0
 for i in lines:
-    try:
+    if c > 4 and c < len(lines) - 3:
         line = i.split("|")
-        item.append(line[1])
-        number.append(line[2])
-    except:
-        continue
+        item.append(line[1].strip())
+        number.append(line[2].strip())
+    c += 1
 for i in item:
     print(i)
 input("------------------")
