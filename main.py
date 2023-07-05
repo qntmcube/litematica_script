0d5b4d69523578769f91dee81c038cdc9933fada
qntmcube <100043837+qntmcube@users.noreply.github.com>
2023-07-04 17:19:14 -0700

    made them write to files

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ file1 = open('file.txt', 'r')
 lines = file1.readlines()
 item = []
 number = []
-c = 0
 for i in lines:
-    if c > 4 and c < len(lines) - 3:
-        line = i.split("|")
+    line = i.split("|")
+    if len(line) == 6 and "Item" not in line[1]:
         item.append(line[1].strip())
         number.append(line[2].strip())
-    c += 1
-for i in item:
-    print(i)
-input("------------------")
-for i in number:
-    print(i)
+with open('output/items.txt', 'w') as f:
+    for i in item:
+        f.write(i + "\n")
+with open('output/amounts.txt', 'w') as f:
+    for i in number:
+        f.write(i + "\n")
